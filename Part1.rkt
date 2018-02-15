6a8a2a8e011aa33489df86c4a2db18892d48cbbf
Peter Fedrizzi <peterfedrizzi@Peters-MacBook.local>
2018-02-15 11:53:43 -0500

    first commit empty file

---
+++ Part1.rkt
@@ -0,0 +1,12 @@
+; Raza Agha, Huvra Mehta, Peter Fedrizzi
+
+; Implements: variables, assignment statements, mathematical expressions, comparison operators, boolean operators, if statements, while statements, and return statements.
+; OPERATORS: +,-,*,/,% (including the unary -)
+; COMPARISON: ==, !=, <, >, <=, >=
+; BOOLEAN: &&, ||, !
+
+; Variables may store values of type int as well as true and false.
+; Not necessary: Error checking on type
+; Not necessary: short-circuit evaluation of && or ||
+
+
