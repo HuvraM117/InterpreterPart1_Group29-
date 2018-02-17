a2110976f81b2055ebba20e55b2a27e99cdb0b42
Raza Agha <waztaz@Waztaz-MacBook-Pro.local>
2018-02-17 11:00:06 -0500

    some commments added

--- Part1.rkt
+++ Part1.rkt
@@ -11,26 +11,11 @@
 
 ;hello
 
+; I'm trying to get the first two statements (var x) (= x 10) to work properly. 
+
 (require "simpleParser.scm")
 
 (parser "firsttest.java")
-
-; Raza Agha, Huvra Mehta, Peter Fedrizzi
-
-; Implements: variables, assignment statements, mathematical expressions, comparison operators, boolean operators, if statements, while statements, and return statements.
-; OPERATORS: +,-,*,/,% (including the unary -)
-; COMPARISON: ==, !=, <, >, <=, >=
-; BOOLEAN: &&, ||, !
-
-; Variables may store values of type int as well as true and false.
-; Not necessary: Error checking on type
-; Not necessary: short-circuit evaluation of && or ||
-
-;hello
-
-(require "simpleParser.scm")
-
-(parser "test.java")
 
 (define state '())
 
@@ -50,6 +35,8 @@ expression)) (M_value_int (operand2 expression))))
 expression)) (M_value_int (operand2 expression))))
       (else (error 'badoperator "illegal arithmetic expression")))))
 
+
+;This function takes a variable and finds the value associated with it on the state. 
 (define get_state_variable
   (lambda (n state)
     (cond
@@ -59,14 +46,21 @@ expression)) (M_value_int (operand2 expression))))
        (cadr state))
       (get_state_variable (n (cdr state))))))
 
-
+;This function acts like assignment. It takes a variable and value and the state and returns the new state.
+;If the variable is already present it changes the value of that variable. Since it return the state, sometimes
+;it would need to be called in place of state in the function we are using it.
 (define set_state_variable
   (lambda (var n state)
     (cond
       ((null? state) (append state  (cons (list var) (list (list n)))))
       ((eq? (caar state) var) (cons (list var) (cdr state)))
       (set_state_variable var n (cdr state)))))
-      
+
+; Trying to make a declaration type function.
+(define declare_state_variable
+  (lambda (var state)
+    (append state  (cons (list var) (list (list '@))))))
+
 
 (define operator
   (lambda (expression)
@@ -79,6 +73,8 @@ expression)) (M_value_int (operand2 expression))))
 (define operand2
   (lambda (expression)
     (get_state_variable (caddr expression) state)))
+
+
 
 
 
