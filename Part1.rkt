4069ccfdfca2da4c069d5acf2af9fd99ccfe60d1
pfedrizzi <fedrizzi.peter@gmail.com>
2018-02-19 21:39:45 -0500

    final-ish draft, someone can add more abstractions if they want

--- Part1.rkt
+++ Part1.rkt
@@ -1,104 +1,68 @@
 ; Interpreter Project - Part 1
 ; Raza Agha, Huvra Mehta, Peter Fedrizzi
-;comment
 
 (require "simpleParser.scm")
-(parser "test.java")
+;(parser "test.java")
+
+;;;;;;;;;;;
 
 ; INTERPRET
 (define interpret
   (lambda (filename)
-    (M_state_main (parser filename) '((return) (null)))))
+    (return_type (get_state_variable 'return (M_state_main (parser filename) '((return) (null))))))) ; initialize state
+
+; Return the proper value
+(define return_type
+  (lambda (x)
+    (cond
+      ((equal? #t x) 'true)
+      ((equal? #f x) 'false)
+      ((number? x) x)
+      (else (error "Return type unknown")))))
+
+;;;;;;;;;;;
 
 ; MAIN
 (define M_state_main
   (lambda (parselist S) ; takes a parse tree and state
     (cond
-      ; this should have a helper that searches for the return variable!
-      ((null? parselist) (get_state_variable 'return S)) ; (error "Finished parsing, no return statement detected!")) ; (M_state_main '() '()) => error
-      ((not (list? (car parselist))) (error "Mistake! Fix M_state_main list handling...")) ; (M_state_main '(var a) '()) => error
-      ((equal? 'return (command parselist)) (M_state_main (cdr parselist) (M_state_return (firstlist parselist) S))) ; return
-      ((equal? 'var (command parselist)) (M_state_main (cdr parselist) (M_state_var (firstlist parselist) S))) ; variable 
-      ((equal? '= (command parselist)) (M_state_main (cdr parselist) (M_state_assign (firstlist parselist) S))) ; assignment
-      ((equal? 'if (command parselist)) (M_state_main (cdr parselist) (M_state_if (firstlist parselist) S))) ; if firstlist
-      ((equal? 'while (command parselist)) (M_state_main (cdr parselist) (M_state_while (firstlist parselist) S))) ; while statement
-      (else (error "What is going on?")))))
+      ((null? parselist) S) ; end of parse tree
+      ((not (list? parselist)) (error "Input cannot be atom")) ; (M_state_main 'var '()) => error
+      ((not (list? (firstlist parselist))) (error "Input cannot be single list")) ; (M_state_main '(var a) '()) => error
+      ((equal? 'return (command parselist)) (M_state_main (nextlist parselist) (M_state_return (firstlist parselist) S))) ; return
+      ((equal? 'var (command parselist)) (M_state_main (nextlist parselist) (M_state_var (firstlist parselist) S))) ; variable 
+      ((equal? '= (command parselist)) (M_state_main (nextlist parselist) (M_state_assign (firstlist parselist) S))) ; assignment
+      ((equal? 'if (command parselist)) (M_state_main (nextlist parselist) (M_state_if (firstlist parselist) S))) ; if
+      ((equal? 'while (command parselist)) (M_state_main (nextlist parselist) (M_state_while (firstlist parselist) S))) ; while
+      (else (error "Something bad happened, broken parser?")))))
 
-; Abstractions for Main
+; Abstractions for M_state_main
 ; Retrieves command in next statement of parse tree
 (define command
   (lambda (parselist)
     (caar parselist)))
 
 ; Retrieves the next statement of parse tree
-(define firstlist ;(statement '((return 150) (return 120))) => (return 150)
+(define firstlist ; (statement '((return 150) (return 120))) => (return 150)
   (lambda (parselist)
     (car parselist)))
 
-; STATE FOR LIST - 2
-(define M_state_list
-  (lambda (statementlist S) ; takes a parse tree and state
-
-        
-      ; this should have a helper that searches for the return variable!
-      ((null? statementlist) S)
-      
-      ((equal? 'return (statecommand statementlist)) (M_state_list (cdr statementlist) (M_state_return (piece1 statementlist) S))) ; return
-      ((equal? 'var (statecommand statementlist)) (M_state_list (cdr statementlist) (M_state_var (piece1 statementlist) S))) ; variable 
-      ((equal? '= (statecommand statementlist)) (M_state_list (cdr statementlist) (M_state_assign (piece statementlist) S))) ; assignment
-      ((equal? 'if (statecommand statementlist)) (M_state_list (cdr statementlist) (M_state_if (firstlist statementlist) S))) ; if firstlist
-      ((equal? 'while (statecommand statementlist)) (M_state_list (cdr statementlist) (M_state_while (firstlist statementlist) S))) ; while statement
-      (else (error "What is going on?"))))
-
-; STATE FOR LIST
-;(define M_state_list
-;  (lambda (statementlist S) ; takes a parse tree and state
-;    (if ((null? statementlist) S)
-;        (if (list? (piece1 statementlist))
-;            ())
-;        (if (list? (piece2 statementList))
-;            ())
-;        (else ()))))
-;        
-;      ; this should have a helper that searches for the return variable!
-;      ((null? statementlist) S)
-;      
-;      ((equal? 'return (statecommand statementlist)) (M_state_list (cdr statementlist) (M_state_return (piece1 statementlist) S))) ; return
-;      ((equal? 'var (statecommand statementlist)) (M_state_list (cdr statementlist) (M_state_var (piece1 statementlist) S))) ; variable 
-;      ((equal? '= (statecommand statementlist)) (M_state_list (cdr statementlist) (M_state_assign (piece statementlist) S))) ; assignment
-;      ((equal? 'if (statecommand statementlist)) (M_state_list (cdr statementlist) (M_state_if (firstlist statementlist) S))) ; if firstlist
-;      ((equal? 'while (statecommand statementlist)) (M_state_list (cdr statementlist) (M_state_while (firstlist statementlist) S))) ; while statement
-;      (else (error "What is going on?")))))
-
-; Abstractions for Main
-; Retrieves command in next statement of parse tree
-(define statecommand
-  (lambda (statementlist)
-    (car statementlist)))
-
-(define piece1
-  (lambda (statementlist)
-    (cadr statementlist)))
-
-(define piece2
-  (lambda (statementlist)
-    (caddr statementlist)))
-          
-
-; !!! - I think we need to change how states are passed for "if" and "while" parts - !!! 
-
+; Returns the subtree of given parse tree
+(define nextlist
+  (lambda (parselist)
+    (cdr parselist)))
+  
 ;;;;;;;;;;;
 
 ; RETURN
-(define M_state_return ; store a return variable and only update if it is still null
+(define M_state_return ; only update return if it has a value of null
   (lambda (statement S)
     (cond
-      ((null? statement) (error "Parser is broken"))
+      ((null? statement) (error "Parser is broken?"))
       ((equal? 'null (get_state_variable 'return S)) (set_state_variable 'return (M_expression (return_expression statement) S) S))
-      (else S)))) ; not necessary?
-      ;((list? (return_expression statement)) (set_state_variable 'return (M_expression (return_expression statement) S) S)) ; change #t to #f for the return
-      ;(else (error "why?"))))) ;(get_state_variable (return_expression statement) S))))) ; not necessary?
+      (else S))))
 
+; Abstractions for M_state_return
 ; Retrieves the expression of a return statement;
 (define return_expression
   (lambda (statement)
@@ -111,9 +75,10 @@
   (lambda (statement S)
     (cond
       ((null? statement) (error "Parser is broken"))
-      ((null? (cddr statement)) (set_state_variable (varname statement) '? S))
+      ((null? (var_list statement)) (set_state_variable (varname statement) '? S))
       (else (set_state_variable (varname statement) (M_expression (var_expression statement) S) S)))))
 
+; Abstractions for M_state_var
 ; Variable name
 (define varname
   (lambda (statement)
@@ -124,15 +89,21 @@
   (lambda (statement)
     (caddr statement)))
 
+; Retrieves the expression list
+(define var_list
+  (lambda (statement)
+    (cddr statement)))
+
 ;;;;;;;;;;;
 
 ; ASSIGNMENT
 (define M_state_assign
   (lambda (statement S)
     (cond
-      ((null? statement) (error "Parse is broken"))
+      ((null? statement) (error "Parser is broken"))
       (else (set_state_variable (varname statement) (M_expression (assign_expression statement) S) S)))))
 
+; Abstractions for M_state_assign
 ; Variable name
 (define varname
   (lambda (statement)
@@ -145,7 +116,7 @@
 
 ;;;;;;;;;;;
     
-; EXPRESSION - EVALUATION
+; EXPRESSION - Evaluates expression
 (define M_expression
   (lambda (express S)
     (cond
@@ -164,12 +135,10 @@
   (lambda (operator oplist)
     (cond
       ((null? oplist) #f)
-      ((equal? operator (car oplist)) #t)
+      ((equal? operator (car oplist)) #t) ; abstraction not applied because it operates on a list
       (else (member? operator (cdr oplist))))))
-
-;;;
     
-; BOOLEAN EXPRESSION - EVALUATION
+; BOOLEAN - Evaluates boolean expression
 (define M_boolean
   (lambda (express S)
     (cond
@@ -185,7 +154,7 @@
       ((equal? '>= (operator express)) (>= (M_expression (exp1 express) S) (M_expression (exp2 express) S)))
       (else (error "Missing operator, bad team!")))))
 
-; VALUE EXPRESSION - EVALUATION
+; VALUE - Evaluates numerical expression
 (define M_value
   (lambda (express S)
     (cond
@@ -198,7 +167,8 @@
       ((equal? '% (operator express)) (remainder (M_expression (exp1 express)S) (M_expression (exp2 express)S)))
       (else (error "illegal arithmetic expression")))))
 
-; Abstractions for evaluation functions
+; Abstractions for M_expression
+; operator type
 (define operator
   (lambda (express)
     (car express)))
@@ -220,17 +190,18 @@
   (lambda (statement S)
     (if (null? (cdddr statement)) ; abstraction needed?
         (if (M_expression (condition statement) S)
-            (M_state_main (then statement) S))
+            (M_state_main (list (then statement)) S)
+            S)
         (if (M_expression (condition statement) S)
-        (M_state_main (then statement) S) ; make a list of lists?
-        (M_state_main (else statement) S)))))
+        (M_state_main (list (then statement)) S) ; make a list of lists?
+        (M_state_main (list (else statement)) S)))))
 
-; Abstractions for if-statement
+; Abstractions for M_state_if
 (define condition
   (lambda (if_statement)
     (cadr if_statement)))
 
-(define then ; Also used for while loop
+(define then ; also used for while loop
   (lambda (if_statement)
     (caddr if_statement)))
 
@@ -244,14 +215,10 @@
 (define M_state_while
   (lambda (statement S)
     (if (M_expression (condition statement) S)
-        (M_state_while statement (M_state_main (then statement) S)))))
+        (M_state_while statement (M_state_main (list (then statement)) S))
+        S)))
 
-; Abstraction for while-statement
-;(define then
-;  (lambda (while_statement)
-;    (caddr while_statment)))
-
-;;;;;;;;;;;
+;;;;;;;;;;;A
  
 ; STATE HANDLING
 
@@ -260,11 +227,9 @@
   (lambda (var S)
     (cond
       ((number? var) var) ; not necessary?
-      ((null? S) (error "Variable not found"))
+      ((null? (car S)) (error "Variable not found"))
       ((eq? (caar S) var) (caadr S)) ; if variable is found return value
       (else (get_state_variable var (cons (cdar S) (list (cdadr S)))))))) ; reduce copy of state recrusively
-
-; Abstraction probably not necessary?
 
 ; This function acts like assignment. It takes a variable and value and the state and returns the new state.
 ; If the variable is already present it changes the value of that variable. Remember state is like ((a b c) (10 11 12)).
@@ -283,15 +248,16 @@
   (lambda (lis1 lis2)
     (cons (append (car lis1) (car lis2)) (list (append (cadr lis1) (cadr lis2))))))
 
+; Abstractions for state handling ???
+
 ;;;;;;;;;;;
 
 ; TESTS
 
-; Not implemented: error checking on type
 ; Not implemented: side effects
+; Not implemented: error checking on type
+; Not implemented: error checking for redefine
 ; Not implemented: short-circuit evaluation of && or ||
-; Not implemented: true instead of #t
-; Not implemented: proper return structure
 
 (parser "test1.java")
 (interpret "test1.java") ; => 150
@@ -303,51 +269,39 @@
 (interpret "test4.java") ; => 16
 (parser "test5.java")
 (interpret "test5.java") ; => 220
-
-; PROBLEM: M_state_main cannot handle '(= x 3) only '((= x 3)(return 'something'))
 (parser "test6.java")
 (interpret "test6.java") ; => 5
-;(parser "test7.java")
-;(interpret "test7.java") ; => 6
-;(parser "test8.java")
-;(interpret "test8.java") ; => 10
-
-; PROBLEM: Not sure.
-;(parser "test9.java")
-;(interpret "test9.java") ; => 5
-
+(parser "test7.java")
+(interpret "test7.java") ; => 6
+(parser "test8.java")
+(interpret "test8.java") ; => 10
+(parser "test9.java")
+(interpret "test9.java") ; => 5
 (parser "test10.java")
 (interpret "test10.java") ; return 6 * -(4 * 2) + 9; => -39
 
 ; PROBLEM: This code should return an error, because y has not been declared
-;(parser "test11.java")
-;(interpret "test11.java") ; => This code should give an error (using before declaring)
+(parser "test11.java")
+(interpret "test11.java") ; => This code should give an error (using before declaring)
 
-; PROBLEM: Assignment with another variable
 ;(parser "test12.java")
 ;(interpret "test12.java") ; => This code should give an error (using before declaring).
-
-; PROBLEM: Error for adding '? to stuff
 ;(parser "test13.java")
 ;(interpret "test13.java") ; => This code should give an error (using before assigning).
 ;(parser "test14.java")
-;(interpret "test14.java") ; => This code should give an error (redefining). This is not a required error, but it would be nice if you could catch these.
-
-; PROBLEM: Fix this error simple error
+(interpret "test14.java") ; => 30 - This is not a required error, this code should give an error (redefining).
 (parser "test15.java")
 (interpret "test15.java") ; => This code should return true (not #t).
-
-;(parser "test16.java")
-;(interpret "test16.java") ; => 100
-;(parser "test17.java")
-;(interpret "test17.java") ; => This code should return false (not #f).
-;(parser "test18.java")
-;(interpret "test18.java") ; => true
-;(parser "test19.java")
-;(interpret "test19.java") ; => 128
-;(parser "test20.java")
-;(interpret "test20.java") ; => 12
-
+(parser "test16.java")
+(interpret "test16.java") ; => 100
+(parser "test17.java")
+(interpret "test17.java") ; => This code should return false (not #f).
+(parser "test18.java")
+(interpret "test18.java") ; => true
+(parser "test19.java")
+(interpret "test19.java") ; => 128
+(parser "test20.java")
+(interpret "test20.java") ; => 12
 
 ; Additional Tests for Students Looking for an Extra Challenge...
 ;(parser "test21.java")
